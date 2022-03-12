0590adb9651a46c8ea90406e30e89c38ad43c011
Molina Davis <44956670+owlsrwise@users.noreply.github.com>
2022-03-12 10:48:43 -0600

    fuel quote tests; confirmed page loads with dummy quote

--- tests/test_fuelQuoteFormValidations.py
+++ tests/test_fuelQuoteFormValidations.py
@@ -1,22 +1,53 @@
 from fuelQuoteFormValidations import *
 
-
-# test erroneous values for Delivery Date, Gallons Requested, Fuel Type
-
-def test_todaysDate():
-    assert not validate('2022-03-06', '-1', '0')
+# test erroneous values for just Delivery Date (other fields valid)
+def test_notAstring():
+    assert not validate(48, '1', 'regUnl')
 
 def test_previousDate():
-    assert not validate('2019-03-06', '-1', '0')
+    assert not validate('2019-03-06', '1', 'regUnl')
 
-def test_ifgallonsnumeric():
-    assert not validate('2022-03-06', '-1', '0')
+def test_DateFormat():
+    assert not validate('06-10-2022', '1', 'regUnl')
+    assert not validate('06/06/2022', '1', 'regUnl')
+    assert not validate('06.06.2022', '1', 'regUnl')
+    assert not validate(None, '1', 'regUnl')
 
-def test_ifgallonslessthanzero():
-    assert not validate('2022-03-06', '-1', '0')
+# test erroneous values for just Gallons Requested (other fields valid)
+def test_notAstring():
+    assert not validate('2022-06-10', 86, 'regUnl')
 
-#validate('00-00-0000', 0.1, 0)
+def test_ifGallonsNumeric():
+    assert not validate('2022-06-10', 'a', 'regUnl')
+    assert not validate('2022-06-10', ' ', 'regUnl')
+    assert not validate('2022-06-10', '', 'regUnl')
+    assert not validate('2022-06-10', '-', 'regUnl')
+    assert not validate('2022-06-10', None, 'regUnl')
 
-#validate('00-00-0000', 0.1, 0)
+def test_ifGallonsEqualsZero():
+    assert not validate('2022-06-10', '0', 'regUnl')
 
+def test_ifGallonsNotFloat():
+    assert not validate('2022-06-10', '0.4', 'regUnl')
 
+def test_ifGallonsLessthanZero():
+    assert not validate('2022-06-10', '-1', 'regUnl')
+    assert not validate('2022-06-10', '-1.3', 'regUnl')
+
+# test erroneous values for just Fuel Type (other fields valid)
+def test_notAstring():
+    assert not validate('2022-06-10', '1', 57)
+
+def test_ifFuelTypeNotInList():
+    assert not validate('2022-06-10', '1', 'Reg')
+    assert not validate('2022-06-10', '1', 'Prem')
+    assert not validate('2022-06-10', '1', 'Diesel')
+    assert not validate('2022-06-10', '1', ' ')
+    assert not validate('2022-06-10', '1', '')
+    assert not validate('2022-06-10', '1', '-')
+    assert not validate('2022-06-10', '1', 'none')
+    assert not validate('2022-06-10', '1', None)
+    assert not validate('2022-06-10', '1', '5')
+
+def test_all_fields_good():
+    assert validate('2022-06-10', '1', 'regUnl')
