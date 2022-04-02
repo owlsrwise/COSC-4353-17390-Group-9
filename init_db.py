f7813f1c73bec00b06c77e62a0f290863d4f79b7
Molina Davis <44956670+owlsrwise@users.noreply.github.com>
2022-04-01 22:49:41 -0500

    tested that profile db query populates quote form and history table

--- init_db.py
+++ init_db.py
@@ -22,28 +22,10 @@ cur.execute('''CREATE TABLE FuelPrices
     regUnl REAL NOT NULL,
     premUnl REAL NOT NULL)''')    # state column will have either 'Texas' or 'other' 
 
-# temporary profile table (pending Manuel's table)
-cur.execute("DROP TABLE IF EXISTS DummyProfile")
-cur.execute('''CREATE TABLE DummyProfile          
-    (id INTEGER PRIMARY KEY AUTOINCREMENT,
-    custId INTEGER NOT NULL,
-    fullName TEXT NOT NULL,
-    address1 TEXT NOT NULL,
-    address2 TEXT NOT NULL,
-    city TEXT NOT NULL,
-    state TEXT NOT NULL,
-    zipcode INTEGER NOT NULL)''') 
 
-# initial values to test database
-cur.execute("INSERT INTO FuelQuoteData (custId, date, gallons, fuel, quote) \
-    VALUES ('001','02/18/2022', 5, 'regUnl', '19.50')")
+#cur.execute("INSERT INTO FuelPrices (state, diesel, regUnl, premUnl) \
+#    VALUES ('Texas', '4.00', '3.90', '4.20')")
 
-cur.execute("INSERT INTO FuelPrices (state, diesel, regUnl, premUnl) \
-    VALUES ('Texas', '4.00', '3.90', '4.20')")
-
-cur.execute("INSERT INTO DummyPRofile (custId, fullName, address1, address2, city, state, zipcode) \
-    VALUES ('001', 'Molina Davis', '2300 Street', 'Apt 5', 'Houston', 'Texas', 77004)")
-    
 conn.commit()
 print ("Records created successfully")
 conn.close()
